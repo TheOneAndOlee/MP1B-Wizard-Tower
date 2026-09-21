be09a763c9fc00c3dabfb55dcfdad8a11adf664e
megumidaisiki <122090781@link.cuhk.edu.cn>
2026-09-21 18:47:57 -0500

    update yunfan's room

---
+++ Assets/Scripts/CollectionManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CollectionManager : MonoBehaviour
+{
+    public const int RequiredNumber = 3;
+
+    private static CollectionManager _instance;
+    public static CollectionManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject objectCM = new GameObject("CollectionManager");
+                _instance = objectCM.AddComponent<CollectionManager>();
+                DontDestroyOnLoad(objectCM);
+            }
+            return _instance;
+        }
+    }
+
+    private readonly HashSet<string> collectedIds = new HashSet<string>();
+    public int TotalCollected
+    {
+        get { return collectedIds.Count; }
+    }
+
+    public void CollectScroll(string scrollId)
+    {
+        collectedIds.Add(scrollId);
+    }
+}
